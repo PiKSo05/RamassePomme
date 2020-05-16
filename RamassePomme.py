5f5a5fbee866480736ecc472b20128e40036adca
PiKSo05 <vilain.vincent@gmail.com>
2020-05-16 11:59:13 +0200

    Update RamassePomme.py
    Ajout de l'écran de GameOver avec appuie sur touce espace pour recommencer

--- RamassePomme.py
+++ RamassePomme.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 #créer un personnage
 panier = Actor('panier', midtop=(400, 500))
@@ -10,13 +11,11 @@ HEIGHT = 600
 APPLE_SPEED = 4
 BASKET_SPEED = 6
 VERT_FLUO = 66, 245, 69
-# DELAY_TIME
 
 #Variables
 score = 0
 nombre_vies = 3
-game_over = True
-wait_timer = 0
+game_over = False
 # highscore = storage.setdefault("highscore", 0)
 
 #on dessine le fond et les acteurs
@@ -28,14 +27,27 @@ def draw():
     panier.draw()
     screen.draw.text("Score " + str(score), (30, 550), color=(VERT_FLUO), fontname="arcadeclassic", fontsize=48)
     screen.draw.text("Vies " + str(nombre_vies), (600, 550), color=(VERT_FLUO), fontname="arcadeclassic", fontsize=48)
+    if game_over:
+        screen.draw.text("GAME OVER", (220, 200), color=(VERT_FLUO), fontname="arcadeclassic", fontsize=72)
+        screen.draw.text("Appuyer sur Espace pour recommencer", (100, 300), color=(VERT_FLUO), fontname="arcadeclassic", fontsize=32)
 
 # on déplace le personnage
 def update():
-    deplacerPanier()
-    deplacerPomme()
+    global wait_timer
+    global game_over
+    global score
+    global nombre_vies
+
     if game_over:
-        return
-        #wait_timer = time.time() + DELAY_TIME
+        if keyboard.space:
+            game_over = False
+            score = 0
+            nombre_vies = 3
+        else:
+            return
+    else:
+        deplacerPanier()
+        deplacerPomme()
 
 #déplacement panier
 def deplacerPanier():
@@ -52,12 +64,15 @@ def deplacerPanier():
 def deplacerPomme():
     global score
     global nombre_vies
+    global game_over
     pomme.bottom += APPLE_SPEED
     if pomme.bottom > HEIGHT:
         sounds.impact_sol.play()
         mettrePommeEnPositionDepart()
         nombre_vies -= 1
         print("Raté! Il te reste = " + str(nombre_vies) + " vies")
+        if nombre_vies <= 0:
+            game_over = True
     if pomme.colliderect(panier):
         sounds.impact_panier.play()
         mettrePommeEnPositionDepart()
