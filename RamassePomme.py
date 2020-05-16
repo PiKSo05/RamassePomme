3b7bbbf0f8a6a0a9926b4fc0e168641d8c576de8
PiKSo05 <vilain.vincent@gmail.com>
2020-05-16 11:37:44 +0200

    Update RamassePomme.py

--- RamassePomme.py
+++ RamassePomme.py
@@ -10,9 +10,13 @@ HEIGHT = 600
 APPLE_SPEED = 4
 BASKET_SPEED = 6
 VERT_FLUO = 66, 245, 69
+# DELAY_TIME
 
 #Variables
 score = 0
+nombre_vies = 3
+game_over = True
+wait_timer = 0
 # highscore = storage.setdefault("highscore", 0)
 
 #on dessine le fond et les acteurs
@@ -23,11 +27,15 @@ def draw():
     pomme.draw()
     panier.draw()
     screen.draw.text("Score " + str(score), (30, 550), color=(VERT_FLUO), fontname="arcadeclassic", fontsize=48)
+    screen.draw.text("Vies " + str(nombre_vies), (600, 550), color=(VERT_FLUO), fontname="arcadeclassic", fontsize=48)
 
 # on déplace le personnage
 def update():
     deplacerPanier()
     deplacerPomme()
+    if game_over:
+        return
+        #wait_timer = time.time() + DELAY_TIME
 
 #déplacement panier
 def deplacerPanier():
@@ -43,10 +51,13 @@ def deplacerPanier():
 #déplacement de la pomme
 def deplacerPomme():
     global score
+    global nombre_vies
     pomme.bottom += APPLE_SPEED
     if pomme.bottom > HEIGHT:
         sounds.impact_sol.play()
         mettrePommeEnPositionDepart()
+        nombre_vies -= 1
+        print("Raté! Il te reste = " + str(nombre_vies) + " vies")
     if pomme.colliderect(panier):
         sounds.impact_panier.play()
         mettrePommeEnPositionDepart()
